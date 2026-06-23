30399732cfb136a1bfa7fe4fd81fc89d9bf4e6a4
Ashikur Rahman <ashikurrahmanvar@gmail.com>
2026-06-23 18:09:46 -0400

    Release 1.0.3

--- apps/backend/api/app_metadata.py
+++ apps/backend/api/app_metadata.py
@@ -1,8 +1,8 @@
 APP_METADATA = {
     "appName": "Ashikur Portfolio",
-    "version": "1.0.2",
-    "releaseDate": "2026-05-26",
-    "releaseLabel": "Nuh (AS)",
+    "version": "1.0.3",
+    "releaseDate": "2026-06-23",
+    "releaseLabel": "Salih (AS)",
 }
 
 PUBLIC_METADATA_KEYS = frozenset(APP_METADATA.keys())
